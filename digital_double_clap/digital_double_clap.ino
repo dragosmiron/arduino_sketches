56ef2448d89037609e644bd076cc058feec818f7
Dragos Miron <dragos.miron@simplybusiness.co.uk>
2016-01-15 22:50:52 +0200

    added readme + some improvements

--- digital_double_clap/digital_double_clap.ino
+++ digital_double_clap/digital_double_clap.ino
@@ -1,14 +1,25 @@
-const bool DEBUG = true;
+/*
+  This sketch reads a piezo element to see if a sound is made (eg. clap)
+  two times in succession after which an action is performed.
+  The code listens for a double clap in a 1.5 second period, after which
+  the whole system gets reset
+
+  For this sketch, the sensor sensitivity is adjusted manually on the board itself.
+  For more granular difference, consider switching to digitalRead to analogRead
+*/
+
+const bool DEBUG = true; //allow serial prints
 
 const int ledPin = 8;     
-const int knockSensor = A0;
+const int soundSensor = A0;
 const int threshold = 200; 
+//these two delays below are required because echo can still trigger the sensor
+const int delayBetweenClaps = 300;
+const int delatForReset = 300;
 const int resetPeriod = 1500; //milliseconds
-
 
 int sensorReading = 1; 
 int ledState = LOW; 
-int state = 0;  
 unsigned long timeOfClap = 0;
 
 enum State {waiting, first_clap, waiting_2nd_clap};
@@ -20,7 +31,7 @@ void setup() {
 }
 
 void loop() {
-  sensorReading = digitalRead(knockSensor);
+  sensorReading = digitalRead(soundSensor);
   
   if (sensorReading == LOW) {
     if (current_state == State::waiting){
@@ -29,13 +40,11 @@ void loop() {
       debugSerialPrintln("State = first clap");
     }
     if (current_state == State::waiting_2nd_clap){
-      // toggle the status of  ledPin
-      ledState = !ledState;
-      digitalWrite(ledPin, ledState);
-      debugSerialPrintln("2xClap!");
+      performAction();
+      
       current_state = State::waiting;
       debugSerialPrintln("State = waiting");
-      delay(300); // delay after which the process resumes
+      delay(delayBetweenClaps); // delay after which the process resumes
     }
   } else {
     if (current_state == State::first_clap) {
@@ -49,6 +58,13 @@ void loop() {
     timeOfClap = 0;
     debugSerialPrintln("Too long for 2nd clap, resetting..");
   }
+}
+
+void performAction(){
+  // toggle the status of  ledPin
+  ledState = !ledState;
+  digitalWrite(ledPin, ledState);
+  debugSerialPrintln("2xClap!");
 }
 
 unsigned long timeFromClap(){
